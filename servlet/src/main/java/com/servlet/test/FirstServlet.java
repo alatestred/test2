965c9327afdcdecd7ed200ff294e2de01513a0d9
ilyas <ilyas.dekiyev@gmail.com>
2020-09-15 19:44:30 +0600

    test

--- servlet/src/main/java/com/servlet/test/FirstServlet.java
+++ servlet/src/main/java/com/servlet/test/FirstServlet.java
@@ -3,12 +3,12 @@ package com.servlet.test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Daulet Zholtayev
@@ -26,16 +26,38 @@ public class FirstServlet extends HttpServlet {
 //        object.addProperty("name", req.getParameter("param"));
 
         Map<String, String> map = new HashMap<String, String>();
-        map.put("name", req.getParameter("param"));
+        map.put("userName", req.getParameter("param"));
 
 
         // download POSTMAN
         // req.getCookies() get and edit
+        String name = req.getParameter("userName");
+        Cookie cook = new Cookie("userName", name);
+        resp.addCookie(cook);
+
+        Cookie sessionId = new Cookie("sessionId", req.getRemoteAddr());
+        resp.addCookie(sessionId);
+
         // req.getHeaderNames() req.getHeader()
+
+        Enumeration headerName = req.getHeaderNames();
+        while (headerName.hasMoreElements()) {
+            System.out.println(headerName.nextElement());
+        }
         //req.getRequestedSessionId()
 
+        Cookie c1 = new Cookie("sessionId", req.getRequestedSessionId());
+        resp.addCookie(c1);
+
+
 //        resp.sendRedirect();
+
+//        resp.sendRedirect("https://www.google.com");
+
 //        resp.sendError();
+//        resp.sendError();
+
+
 
         resp.getWriter().println(new ObjectMapper().writeValueAsString(map));
     }
