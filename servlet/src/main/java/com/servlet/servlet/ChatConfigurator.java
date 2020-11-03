768c336bbc4b33c32e2d0cb8e98c958186941f09
Daulet Zholtayev <zholtayev@gmail.com>
2020-11-03 20:21:51 +0600

    web sockets

---
+++ servlet/src/main/java/com/servlet/servlet/ChatConfigurator.java
@@ -0,0 +1,25 @@
+package com.servlet.servlet;
+
+import com.servlet.domain.User;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+/**
+ * @author Daulet Zholtayev
+ * @since 03.11.2020 - 20:01
+ */
+public class ChatConfigurator extends ServerEndpointConfig.Configurator {
+
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        HttpSession httpSession = (HttpSession)request.getHttpSession();
+        User user = (User)httpSession.getAttribute("user");
+        if(user == null) {
+            return;
+        }
+        sec.getUserProperties().put("userId", user.getId());
+    }
+}
--- servlet/src/main/java/com/servlet/servlet/ChatServer.java
+++ servlet/src/main/java/com/servlet/servlet/ChatServer.java
@@ -1,27 +1,37 @@
 package com.servlet.servlet;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.servlet.service.MessagesService;
 
+import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import javax.websocket.server.ServerEndpointConfig;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
-@ServerEndpoint("/wsChat")
+@ServerEndpoint(value = "/wsChat", configurator = ChatConfigurator.class)
 public class ChatServer {
 
+    // interface
+    // abstract
+
     static Set<Session> sessions = new HashSet<>();
+
+    EndpointConfig config;
+
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session, EndpointConfig config) {
+        Long userId = (Long) config.getUserProperties().get("userId");
+        this.config = config;
+        session.getUserProperties().put("userId", userId);
         sessions.add(session);
     }
 
@@ -32,10 +42,21 @@ public class ChatServer {
 
     @OnMessage
     public void onMessage(String message, Session session) throws IOException {
-//        session.getBasicRemote().sendText(message);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.readValue();
-//        JsonParser parser = new JsonParser();
-//        JsonElement parsedObj = parser.(message);
+        JsonParser parser = new JsonParser();
+        JsonObject obj = parser.parse(message).getAsJsonObject();
+        long chatId = obj.get("chatId").getAsLong();
+        String chatMessage = obj.get("message").getAsString();
+
+        Long userId = (Long) config.getUserProperties().get("userId");
+
+        try {
+            MessagesService.setMessage( chatMessage, chatId, userId);
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+
+        for (Session local : sessions) {
+            local.getBasicRemote().sendText(message);
+        }
     }
 }
